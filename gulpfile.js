aa14a8401f7bc3e4b70cad72a86d4899bee309cf
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-02-05 11:21:04 +0100

    Working on exposing both loader and profile

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var PROD 		= JSON.parse(process.env.PROD_DEV || "0");
 //
 // Compile the sources 
 //
-gulp.task('dist', function() {
+gulp.task('dist/jbb-profile-three', function() {
 	return gulp.src('index.js')
 		.pipe(webpack({
 			module: {
@@ -19,26 +19,72 @@ gulp.task('dist', function() {
 		    },
 		    output: {
 		    	// The output filename
-		    	filename: 'jbb-profile-three.min.js',
+		    	filename: PROD ? 'jbb-profile-three.min.js' : 'jbb-profile-three.js',
 				// Export itself to a global var
 				libraryTarget: 'var',
 				// Name of the global var: 'Foo'
 				library: 'JBBProfileThree'
 			},
 			externals: {
-				'jquery': 'jQuery',
 				'three': 'THREE',
 			},
-		    plugins: [
+		    plugins: PROD ? [
 		    	new webpack.webpack.optimize.DedupePlugin(),
 			    new webpack.webpack.optimize.UglifyJsPlugin({
 			    	minimize: true
 			    })
+		    ] : [
+		    	new webpack.webpack.optimize.DedupePlugin(),
 		    ]
 		}))
 		.pipe(header("/* THREE.js profile for JBB - https://github.com/wavesoft/jbb-profile-three */\n"))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist/jbb-profile-three'));
 });
+
+//
+// Compile the sources 
+//
+gulp.task('dist/jbb-profile-three-loader', function() {
+	return gulp.src('loader.js')
+		.pipe(webpack({
+			module: {
+				loaders: [
+					{ test: /\.json$/, loader: 'json' },
+				],
+		    },
+		    node: {
+		    	'fs': 'empty'
+		    },
+		    output: {
+		    	// The output filename
+		    	filename: PROD ? 'jbb-profile-three-loader.min.js' : 'jbb-profile-three-loader.js',
+				// Export itself to a global var
+				libraryTarget: 'var',
+				// Name of the global var: 'Foo'
+				library: 'JBBProfileThreeLoader'
+			},
+			externals: {
+				'three': 'THREE',
+			},
+		    plugins: PROD ? [
+		    	new webpack.webpack.optimize.DedupePlugin(),
+			    new webpack.webpack.optimize.UglifyJsPlugin({
+			    	minimize: true
+			    })
+		    ] : [
+		    	new webpack.webpack.optimize.DedupePlugin(),
+		    ]
+		}))
+		.pipe(header("/* THREE.js profile loader for JBB - https://github.com/wavesoft/jbb-profile-three */\n"))
+		.pipe(gulp.dest('dist/jbb-profile-three'));
+});
+
+// The files to pack on dist release
+gulp.task('dist', [
+	'dist/jbb-profile-three',
+	'dist/jbb-profile-three-loader'
+]);
+
 
 // By default run only script
 gulp.task('default', ['dist']);
