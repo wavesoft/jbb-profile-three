89b9931f63134a738f64d08a82b61a7775cb9a26
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-05-27 23:14:09 +0200

    Fixing a loader bug

--- dist/jbb-profile-three-loader.js
+++ dist/jbb-profile-three-loader.js
@@ -381,9 +381,6 @@ var JBBProfileThreeLoader =
 	var queueIndex = -1;
 
 	function cleanUpNextTick() {
-	    if (!draining || !currentQueue) {
-	        return;
-	    }
 	    draining = false;
 	    if (currentQueue.length) {
 	        queue = currentQueue.concat(queue);
